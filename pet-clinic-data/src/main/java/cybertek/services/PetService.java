c1d0b10bbea0b25255850a1180872443b00ff56a
ozzy <ozzy@cybertekschool.com>
2020-05-14 12:13:19 -0400

    Implement Interfaces for Services. Closes #5

---
+++ pet-clinic-data/src/main/java/cybertek/services/PetService.java
@@ -0,0 +1,12 @@
+package cybertek.services;
+
+import cybertek.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+    Pet save(Pet owner);
+    Set<Pet> findAll();
+}
