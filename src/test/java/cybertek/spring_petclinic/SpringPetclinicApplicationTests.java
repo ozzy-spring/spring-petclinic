af6628f459910d113721b633c076e79e51ec1bdd
ozzy <ozzy@cybertekschool.com>
2020-05-14 04:48:40 -0400

    Initial Setup

---
+++ src/test/java/cybertek/spring_petclinic/SpringPetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package cybertek.spring_petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetclinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
